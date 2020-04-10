4c804193680a70ad2f6814e68fcbe96bc26b741b
William C. Canin <william.costa.canin@gmail.com>
2020-04-09 21:12:16 -0300

    Correting bug

--- zshpower/commands/deactivate.py
+++ zshpower/commands/deactivate.py
@@ -14,12 +14,13 @@ class DeactivateCommand(Base):
     def main(self, arguments, *, theme_name="robbyrussell"):
         checking_init(self.themes_folder)
 
-        if not read_zshrc(self.zsh_rc)[0] == "zshpower":
-            printer("Already disabled. Nothing to do.", foreground=FG.GREEN)
-            exit(0)
-        if not arguments["--theme"]:
-            change_theme_in_zshrc(self.zsh_rc, theme_name)
-        else:
-            change_theme_in_zshrc(self.zsh_rc, arguments["--theme"])
-        printer("Deactivation process finish.", foreground=FG.FINISH)
-        reload_zsh()
+        if read_zshrc(self.zsh_rc):
+            if not read_zshrc(self.zsh_rc)[0] == "zshpower":
+                printer("Already disabled. Nothing to do.", foreground=FG.GREEN)
+                exit(0)
+            if not arguments["--theme"]:
+                change_theme_in_zshrc(self.zsh_rc, theme_name)
+            else:
+                change_theme_in_zshrc(self.zsh_rc, arguments["--theme"])
+            printer("Deactivation process finish.", foreground=FG.FINISH)
+            reload_zsh()
