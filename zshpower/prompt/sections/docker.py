dec455dbed4ff8befe3a9a1fe03ccb40c4b72830
William C. Canin <william.costa.canin@gmail.com>
2020-04-14 03:34:08 -0300

    Adding section Docker for Prompt

---
+++ zshpower/prompt/sections/docker.py
@@ -0,0 +1,50 @@
+from os import getcwd
+from subprocess import check_output
+from os.path import isfile, join
+from .lib.utils import Color, symbol_ssh, separator, element_spacing
+from zshpower.utils.process import systemctl_is_active
+from zshpower.utils.check import is_tool
+
+
+class Docker(Color):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.dockerfile = join(getcwd(), "Dockerfile")
+        self.docker_compose = join(getcwd(), "docker-compose.yml")
+        self.docker_version_enable = config["docker"]["version"]["enable"]
+        self.docker_version_micro_enable = config["docker"]["version"]["micro"]["enable"]
+        self.docker_symbol = symbol_ssh(config["docker"]["symbol"], "Dkr-")
+        self.docker_color = config["docker"]["color"]
+        self.docker_prefix_color = config["docker"]["prefix"]["color"]
+        self.docker_prefix_text = element_spacing(config["docker"]["prefix"]["text"])
+
+    def get_version(self, space_elem=" "):
+        docker_v = check_output(
+            "docker version --format '{{.Server.Version}}'",
+            shell=True,
+            universal_newlines=True,
+        ).replace("\n", "").split(".")
+        if not self.docker_version_micro_enable:
+            return f"{'.'.join(docker_v[:-1])}{space_elem}"
+        return f"{'.'.join(docker_v)}{space_elem}"
+
+    def __str__(self):
+        if self.docker_version_enable:
+            if (
+                is_tool("docker")
+                and systemctl_is_active("docker")[0] == "active"
+                and isfile(self.dockerfile)
+                or isfile(self.docker_compose)
+            ):
+                docker_prefix = (
+                    f"{Color(self.docker_prefix_color)}"
+                    f"{self.docker_prefix_text}{Color().NONE}"
+                )
+                docker_export = (
+                    f"{separator(self.config)}{docker_prefix}"
+                    f"{Color(self.docker_color)}"
+                    f"{self.docker_symbol}{self.get_version()}{Color().NONE}"
+                )
+                return str(docker_export)
+        return ""
