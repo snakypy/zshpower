09cbe854db3ae81f57f451ec8329a92a3fc08e32
William C. Canin <william.costa.canin@gmail.com>
2020-04-14 03:52:57 -0300

    Adding error output to the input

--- zshpower/prompt/sections/command.py
+++ zshpower/prompt/sections/command.py
@@ -5,6 +5,8 @@ class Command:
     def __init__(self, config):
         self.config = config
         self.cmd_symbol = symbol_ssh(config["command"]["symbol"], "> ")
+        self.cdm_error_symbol = symbol_ssh(config["command"]["error"]["symbol"], "x ")
+        self.cdm_error_color = config["command"]["error"]["color"]
         self.cmd_color = config["command"]["color"]
         self.cmd_new_line = config["command"]["new_line"]["enable"]
 
@@ -12,6 +14,10 @@ class Command:
         if not self.cmd_new_line:
             jump_line = ""
         cmd_export = (
-            f"{jump_line}{Color(self.cmd_color)}" f"{self.cmd_symbol}{Color().NONE}"
+            f"{jump_line}{Color(self.cmd_color)}"
+            f"%(?.{self.cmd_symbol}."
+            f"{Color(self.cdm_error_color)}"
+            f"{self.cdm_error_symbol})"
+            f"{Color().NONE}"
         )
         return str(cmd_export)
