9058beea186e198319c9978373c3b5d827d4b9e5
William C. Canin <william.costa.canin@gmail.com>
2021-04-24 00:16:33 -0300

    :heavy_plus_sign: Adding decorator runtime and silent_errors

--- zshpower/utils/decorators.py
+++ zshpower/utils/decorators.py
@@ -11,3 +11,32 @@ def assign_cli(arguments, command):
         return wrapper
 
     return decorator
+
+
+def runtime(func):
+    """Decorator to test runtime others functions.
+
+    Returns:
+        str: Return print
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        from datetime import datetime
+
+        start_time = datetime.now()
+        context = func(*args, **kwargs)
+        print(f"Time taken: {datetime.now() - start_time}")
+        return context
+
+    return wrapper
+
+
+def silent_errors(func):
+    from contextlib import suppress
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with suppress(NameError, TypeError, KeyboardInterrupt):
+            return func(*args, **kwargs)
+    return wrapper
