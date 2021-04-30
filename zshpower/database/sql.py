2135f2c3af0c958f58115822a27d7d42db618110
William C. Canin <william.costa.canin@gmail.com>
2021-04-30 19:22:58 -0300

    :fire: Removed file zshpower/database/sql_inject.py

---
+++ zshpower/database/sql.py
@@ -0,0 +1,12 @@
+def sql() -> dict:
+    data = {
+        f"main": (
+            f"CREATE TABLE IF NOT EXISTS `main` ("
+            "  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+            "  name TEXT(100) NOT NULL,"
+            "  version TEXT(50) NOT NULL,"
+            "  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+            ")"
+        )
+    }
+    return data
