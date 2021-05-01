a8855b7658e72953a5aa690d4fea905ff2617428
William C. Canin <william.costa.canin@gmail.com>
2021-05-01 04:27:23 -0300

    :hammer: refactor code.

--- zshpower/database/sql.py
+++ zshpower/database/sql.py
@@ -1,6 +1,6 @@
 def sql() -> dict:
     data = {
-        f"tbl_main": (
+        "tbl_main": (
             "CREATE TABLE IF NOT EXISTS `main` ("
             "  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
             "  name TEXT(100) NOT NULL,"
