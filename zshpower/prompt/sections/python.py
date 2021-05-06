a31f8b28e741d3432220c4d93ec8fc5742fc150d
William C. Canin <william.costa.canin@gmail.com>
2021-05-06 19:36:32 -0300

    :hammer: :bug: Transporting Virtualenv class to the python.py module and solving bug when showing virtual machine

--- zshpower/prompt/sections/python.py
+++ zshpower/prompt/sections/python.py
@@ -1,9 +1,10 @@
 from sys import version_info as sys_version_info
-from zshpower.prompt.sections.lib.utils import symbol_ssh, element_spacing
-from zshpower.prompt.sections.lib.utils import Color, separator
 from zshpower.utils.catch import find_objects
 from zshpower.utils.check import is_tool
-from os import environ as os_environ, getcwd
+from os import environ, getcwd
+from os.path import isfile, join
+from zshpower.prompt.sections.lib.utils import symbol_ssh, element_spacing
+from zshpower.prompt.sections.lib.utils import Color, separator
 
 
 class Python:
@@ -45,7 +46,7 @@ class Python:
                         folders=self.folders,
                         extension=self.extensions,
                     )
-                    or "VIRTUAL_ENV" in os_environ
+                    or "VIRTUAL_ENV" in environ
                 ):
                     prefix = (
                         f"{Color(self.prefix_color)}{self.prefix_text}{Color().NONE}"
@@ -56,6 +57,71 @@ class Python:
                         f"{Color(self.color)}{self.symbol}"
                         f"{self.get_version()}{Color().NONE}"
                     )
+        return ""
+
+
+class Virtualenv:
+    def __init__(self, config):
+        self.config = config
+        self.enable = config["python"]["virtualenv"]["enable"]
+        self.hash_enable = config["python"]["virtualenv"]["poetry"]["hash"]["enable"]
+        self.py_enable = config["python"]["virtualenv"]["poetry"]["py"]["enable"]
+        self.symbol = symbol_ssh(config["python"]["virtualenv"]["symbol"], "")
+        self.involved = config["python"]["virtualenv"]["involved"]
+        self.color = config["python"]["virtualenv"]["color"]
+        self.prefix_color = config["python"]["virtualenv"]["prefix"]["color"]
+        self.prefix_text = element_spacing(config["python"]["virtualenv"]["prefix"]["text"])
+        self.name_enable = config["python"]["virtualenv"]["name"]["normal"]["enable"]
+        self.name_text = config["python"]["virtualenv"]["name"]["text"]
+
+    def get_virtualenv(self) -> str:
+        if "VIRTUAL_ENV" in environ:
+            venv_path = environ["VIRTUAL_ENV"]
+            # The "venv_hash" option is only for virtual machines created with
+            # poetry, where it generates a hash in the name of the virtual machine.
+            if not self.hash_enable and isfile(join(getcwd(), "pyproject.toml")):
+                venv_path = environ["VIRTUAL_ENV"].split("/")[-1]
+                if "-" in venv_path:
+                    return "-".join(venv_path.split("-")[:-2])
+            # The "pg_version" option is only for virtual machines created with
+            # poetry, where it generates the current version of Python in the name
+            # of the virtual machine.
+            if not self.py_enable and isfile(join(getcwd(), "pyproject.toml")):
+                if "-" in venv_path:
+                    return "-".join(venv_path.split("/")[-1].split("-")[:-1])
+
+            return venv_path.split("/")[-1]
+        return ""
+
+    def __str__(self, space_elem=" "):
+        involved_prefix = ""
+        involved_suffix = ""
+
+        if self.enable:
+            if "VIRTUAL_ENV" in environ:
+                prefix = (
+                    f"{Color(self.prefix_color)}" f"{self.prefix_text}{Color().NONE}"
+                )
+
+                if len(self.involved) == 2:
+                    involved_prefix = self.involved[0]
+                    involved_suffix = self.involved[1]
+
+                if self.name_enable:
+                    ret = (
+                        f"{separator(self.config)}{prefix}{Color(self.color)}"
+                        f"{self.symbol}"
+                        f"{involved_prefix}{self.get_virtualenv()}{involved_suffix}"
+                        f"{space_elem}{Color().NONE}"
+                    )
+                else:
+                    ret = (
+                        f"{separator(self.config)}{prefix}{Color(self.color)}"
+                        f"{self.symbol}"
+                        f"{involved_prefix}{self.name_text}{involved_suffix}"
+                        f"{space_elem}{Color().NONE}"
+                    )
+                return str(ret)
         return ""
 
 
