849ff1e0d14598232ca8e810d85b44d13643fe72
William C. Canin <william.costa.canin@gmail.com>
2021-05-17 20:05:27 -0300

    :hammer: Update tests

--- tests/test_zshpower.py
+++ tests/test_zshpower.py
@@ -1,7 +1,14 @@
 """Tests for `zshpower` package."""
 
+from tomlkit import parse as toml_parsed
+from snakypy.helpers.files import read_file
+from os.path import join, exists
+from os import getcwd
 from snakypy.zshpower import __version__
 
 
 def test_version():
-    assert __version__ == "0.7.2"
+    pyproject = join(getcwd(), "pyproject.toml")
+    if exists(pyproject):
+        version_toml = toml_parsed(read_file(pyproject))["tool"]["poetry"]["version"]
+        assert version_toml == __version__
