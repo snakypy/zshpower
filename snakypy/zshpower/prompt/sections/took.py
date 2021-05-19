f54926784432bc70260237069f96e387c4a3c04b
William C. Canin <william.costa.canin@gmail.com>
2021-05-19 13:09:27 -0300

    :hammer: Refactoring to show minutes

--- snakypy/zshpower/prompt/sections/took.py
+++ snakypy/zshpower/prompt/sections/took.py
@@ -3,21 +3,40 @@ from snakypy.zshpower.prompt.sections.lib.utils import Color
 
 
 class Took:
-    def __init__(self, config, timer_took=0):
+    def __init__(self, config, timer_took=""):
         self.enable = config["took"]["enable"]
         self.symbol = symbol_ssh(config["took"]["symbol"], "")
         self.text = config["took"]["text"]
         self.color = config["took"]["color"]
         self.involved = config["took"]["involved"]
         self.show_greater_than = config["took"]["show_greater_than"]
-        self.timer_took = timer_took
+        self.timer_took = str(timer_took)
+
+    def format_timer(self) -> str:
+        if self.timer_took and len(self.timer_took) == 7:
+            if self.timer_took[0] == "0" and self.timer_took[4] == "0":
+                return f"{self.timer_took[1:3]} {self.timer_took[5:]}"
+            elif self.timer_took[0] == "0":
+                return f"{self.timer_took[1:3]} {self.timer_took[4:]}"
+            elif self.timer_took[4] == "0":
+                return f"{self.timer_took[0:3]} {self.timer_took[5:]}"
+        else:
+            if self.timer_took[0] == "0":
+                return self.timer_took[1:3]
+        return self.timer_took
 
     def __str__(self):
         if self.enable:
 
-            if int(self.timer_took) > int(self.show_greater_than):
+            verify_seconds = f"{self.format_timer()[:-1]}"
 
-                timer_took = f"{Color().NONE}{self.timer_took}s"
+            if len(str(self.timer_took)) == 7:
+                verify_seconds = f"{self.format_timer().split()[1].replace('s', '')}"
+
+            if verify_seconds > str(self.show_greater_than):
+
+                timer_took = f"{Color().NONE}{self.format_timer()}"
+
                 timer_took_format = (
                     f" {Color(self.color)}{self.symbol}{self.text} "
                     f"{timer_took}{Color().NONE}"
