d0bb7554432d11cbb7a58f07b1902eab167dc7fc
William C. Canin <william.costa.canin@gmail.com>
2021-05-20 01:18:25 -0300

    :hammer: Formatting and improving logic in show took

--- snakypy/zshpower/prompt/sections/took.py
+++ snakypy/zshpower/prompt/sections/took.py
@@ -2,52 +2,54 @@ from snakypy.zshpower.prompt.sections.lib.utils import symbol_ssh
 from snakypy.zshpower.prompt.sections.lib.utils import Color
 
 
+
 class Took:
-    def __init__(self, config, timer_took=""):
+    def __init__(self, config, took=0):
         self.enable = config["took"]["enable"]
         self.symbol = symbol_ssh(config["took"]["symbol"], "")
         self.text = config["took"]["text"]
         self.color = config["took"]["color"]
         self.involved = config["took"]["involved"]
         self.show_greater_than = config["took"]["show_greater_than"]
-        self.timer_took = str(timer_took)
+        self.took = str(took)
 
-    def format_timer(self) -> str:
-        if self.timer_took and len(self.timer_took) == 7:
-            if self.timer_took[0] == "0" and self.timer_took[4] == "0":
-                return f"{self.timer_took[1:3]} {self.timer_took[5:]}"
-            elif self.timer_took[0] == "0":
-                return f"{self.timer_took[1:3]} {self.timer_took[4:]}"
-            elif self.timer_took[4] == "0":
-                return f"{self.timer_took[0:3]} {self.timer_took[5:]}"
+
+    def format_took(self) -> str:
+        if self.took and len(self.took) == 7:
+            if self.took[0] == "0" and self.took[4] == "0":
+                return f"{self.took[1:3]} {self.took[5:]}"
+            elif self.took[0] == "0":
+                return f"{self.took[1:3]} {self.took[4:]}"
+            elif self.took[4] == "0":
+                return f"{self.took[0:3]} {self.took[5:]}"
         else:
-            if self.timer_took[0] == "0":
-                return self.timer_took[1:3]
-        return self.timer_took
+            if self.took[0] == "0":
+                return self.took[1:3]
+        return self.took
+
+
+    def show(self):
+        timer_took_format = (
+            f" {Color(self.color)}{self.symbol}{self.text} "
+            f"{Color().NONE}{self.format_took()}"
+        )
+
+        if len(self.involved) == 2:
+            timer_took_format = (
+                f" {self.involved[0]}{Color(self.color)}{self.symbol}{self.text} "
+                f"{Color().NONE}{self.format_took()}{self.involved[1]}"
+            )
+
+        return timer_took_format
+
 
     def __str__(self):
         if self.enable:
-
-            verify_seconds = f"{self.format_timer()[:-1]}"
-
-            if len(str(self.timer_took)) == 7:
-                verify_seconds = f"{self.format_timer().split()[1].replace('s', '')}"
-
-            if verify_seconds > str(self.show_greater_than):
-
-                timer_took = f"{Color().NONE}{self.format_timer()}"
-
-                timer_took_format = (
-                    f" {Color(self.color)}{self.symbol}{self.text} "
-                    f"{timer_took}{Color().NONE}"
-                )
-
-                if len(self.involved) == 2:
-                    timer_took_format = (
-                        f" {self.involved[0]}{Color(self.color)}{self.symbol}{self.text} "
-                        f"{timer_took}{Color().NONE}{self.involved[1]}"
-                    )
-
-                return timer_took_format
+            if len(str(self.took)) == 7:
+                return self.show()
+            else:
+                verify_seconds = f"{self.format_took()[:-1]}"
+                if verify_seconds > str(self.show_greater_than):
+                    return self.show()
 
         return ""
