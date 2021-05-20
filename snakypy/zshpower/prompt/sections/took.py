7cf38c5efed45976244bec17bf1b00c3d0402895
William C. Canin <william.costa.canin@gmail.com>
2021-05-20 07:52:30 -0300

    :hammer: Adding typing and formatting variables.

--- snakypy/zshpower/prompt/sections/took.py
+++ snakypy/zshpower/prompt/sections/took.py
@@ -1,9 +1,10 @@
+from typing import Any
 from snakypy.zshpower.prompt.sections.lib.utils import symbol_ssh
 from snakypy.zshpower.prompt.sections.lib.utils import Color
 
 
 class Took:
-    def __init__(self, config, took=0):
+    def __init__(self, config: dict, took: Any = 0):
         self.enable = config["took"]["enable"]
         self.symbol = symbol_ssh(config["took"]["symbol"], "")
         self.text = config["took"]["text"]
@@ -26,18 +27,16 @@ class Took:
         return self.took
 
     def show(self):
-        timer_took_format = (
+        took_format = (
             f" {Color(self.color)}{self.symbol}{self.text} "
             f"{Color().NONE}{self.format_took()}"
         )
-
         if len(self.involved) == 2:
-            timer_took_format = (
+            took_format = (
                 f" {self.involved[0]}{Color(self.color)}{self.symbol}{self.text} "
                 f"{Color().NONE}{self.format_took()}{self.involved[1]}"
             )
-
-        return timer_took_format
+        return took_format
 
     def __str__(self):
         if self.enable:
