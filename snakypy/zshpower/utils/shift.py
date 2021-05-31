4e9c6a3f0646d66315ec9ffa697b89ee76df2b06
William C. Canin <william.costa.canin@gmail.com>
2021-05-31 18:02:40 -0300

    :hammer: Changing url download fonts

--- snakypy/zshpower/utils/shift.py
+++ snakypy/zshpower/utils/shift.py
@@ -121,7 +121,7 @@ def omz_install_plugins(omz_root, plugins):
 
 def install_fonts(home, *, force=False) -> bool:
     url = "https://github.com/snakypy/assets"
-    base_url = "blob/master/zshpower/fonts/fonts.zip?raw=true"
+    base_url = "blob/master/zshpower/fonts/terminal/fonts.zip?raw=true"
     font_name = "DejaVu Sans Mono Nerd Font"
     fonts_dir = join(home, ".fonts")
     snakypy_path_create(fonts_dir)
