c3f0d6ab0fa7abc43091634d04e5a86f795d2cb7
William C. Canin <william.costa.canin@gmail.com>
2021-05-31 23:45:16 -0300

    :bookmark: Setting version 0.8.2

--- snakypy/zshpower/__init__.py
+++ snakypy/zshpower/__init__.py
@@ -18,7 +18,7 @@ with suppress(KeyboardInterrupt):
     HOME = str(Path.home())
     __info__ = {
         "name": "ZSHPower",
-        "version": "0.8.1",
+        "version": "0.8.2",
         "description": "ZSHPower is a theme for ZSH with a manager.",
         "pkg_name": "zshpower",
         "executable": "zshpower",
