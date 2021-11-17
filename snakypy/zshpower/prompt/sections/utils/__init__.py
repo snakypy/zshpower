7187cc7e15b3ae41a60e280e2a7b1020cd9451e5
William C. Canin <william.costa.canin@gmail.com>
2021-11-17 17:06:03 -0300

    :hammer: Implementing logging code in the set method

--- snakypy/zshpower/prompt/sections/utils/__init__.py
+++ snakypy/zshpower/prompt/sections/utils/__init__.py
@@ -2,6 +2,8 @@ from os import getcwd, environ
 from subprocess import check_output
 from typing import List, Union
 
+from snakypy.helpers.catches.finders import is_tool
+
 from snakypy.zshpower.database.dao import DAO
 from snakypy.zshpower.utils.catch import verify_objects
 
@@ -109,9 +111,9 @@ class Version(DAO):
                 )
         return ""
 
-    def set(self, version, key="", action=None) -> bool:
-        if action:
-            # Conditions
+    def set(self, command, version, exec="", key="", action=None) -> bool:
+        if is_tool(exec) and action:
+            # Conditions to save in database
             if action == "insert":
                 query = DAO().select_where(
                     self.tbl_main, key, "name", select=("version",)
@@ -125,5 +127,20 @@ class Version(DAO):
 
             elif action == "update":
                 DAO().update(self.tbl_main, "version", version, "name", key)
+
+            # Register logs
+            if command.returncode != 0:
+                self.log.record(
+                    f"{key.title()} version not registered: {command.stderr}",
+                    colorize=True,
+                    level="error",
+                )
+            elif command.returncode == 0:
+                self.log.record(
+                    f"{key.title()} {version} registered in the database!",
+                    colorize=True,
+                    level="info",
+                )
+
             return True
         return False
