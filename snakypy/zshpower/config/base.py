4ccf89a3195f3d2b0c5799d9f4a6e77fcd124920
William C. Canin <william.costa.canin@gmail.com>
2021-11-20 15:45:12 -0300

    :hammer: Adding path bin

--- snakypy/zshpower/config/base.py
+++ snakypy/zshpower/config/base.py
@@ -13,6 +13,7 @@ class Base:
         self.zshpower_home = join(self.HOME, f".{__info__['pkg_name']}")
         self.tbl_main = [item for item in sql().keys()][0]
         self.config_root = join(self.zshpower_home, "config")
+        self.bin_root = join(self.zshpower_home, "bin")
         self.config_file = join(self.config_root, "zshpower.toml")
         self.database_root = join(self.zshpower_home, ".database")
         self.cache_root = join(self.zshpower_home, ".cache")
@@ -20,7 +21,7 @@ class Base:
         self.database_path = join(self.database_root, "zshpower.sqlite3")
         self.sync_path = f"/usr/local/bin/{__info__['pkg_name']}_sync.sh"
         self.cron_path = f"/etc/cron.d/{__info__['pkg_name']}_task.sh"
-        self.init_file = join(self.zshpower_home, "bin", "zshpower.sh")
+        self.init_file = join(self.bin_root, "zshpower.sh")
         self.zsh_rc = join(self.HOME, ".zshrc")
         self.omz_root = join(self.HOME, ".oh-my-zsh")
         self.themes_folder = join(self.omz_root, "custom/themes")
