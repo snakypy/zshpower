cd420e6734e3a85a96757379b586bb8e38570538
William C. Canin <william.costa.canin@gmail.com>
2021-11-20 00:16:07 -0300

    :star: Adding file for list themes in Oy My ZSH

---
+++ snakypy/zshpower/commands/themes.py
@@ -0,0 +1,25 @@
+from snakypy.helpers.ansi import FG
+from snakypy.helpers.console.display import printer
+from snakypy.zshpower.config.base import Base
+from snakypy.zshpower.utils.catch import read_zshrc_omz
+from snakypy.zshpower.utils.check import checking_init
+
+
+# TODO: Version 0.12.0
+class Theme(Base):
+    def __init__(self, home):
+        Base.__init__(self, home)
+
+    def run(self, arguments) -> None:
+        checking_init(self.HOME, self.logfile)
+
+        zshrc_omz = read_zshrc_omz(self.zsh_rc, self.logfile)
+
+        if not zshrc_omz:
+            return printer(
+                "Command not available. Only with Oh My ZSH installed. Aborted.",
+                foreground=FG().WARNING,
+            )
+
+        if zshrc_omz and arguments["--list"]:
+            ...
