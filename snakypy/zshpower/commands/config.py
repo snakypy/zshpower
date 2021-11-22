f447b4919958934bdcd929fbfe810a65a367070c
William C. Canin <william.costa.canin@gmail.com>
2021-11-22 03:28:52 -0300

    :hammer: Refactoring code.

--- snakypy/zshpower/commands/config.py
+++ snakypy/zshpower/commands/config.py
@@ -1,24 +1,10 @@
-from os import environ
-from pydoc import pager as pydoc_pager
-from shutil import which as shutil_which
-from subprocess import call as subprocess_call
+from pydoc import pager
 
-from snakypy.helpers.catches.finders import is_tool
 from snakypy.helpers.files import read_file
-from tomlkit import parse as toml_parse
 
 from snakypy.zshpower.config.base import Base
-from snakypy.zshpower.utils.catch import get_key
 from snakypy.zshpower.utils.check import checking_init
-
-
-def editor_run(editor, config) -> bool:
-    if shutil_which(editor):
-        get_editor = environ.get("EDITOR", editor)
-        with open(config) as f:
-            subprocess_call([get_editor, f.name])
-            return True
-    return False
+from snakypy.zshpower.utils.process import open_file_with_editor
 
 
 class ConfigCommand(Base):
@@ -29,22 +15,9 @@ class ConfigCommand(Base):
         checking_init(self.HOME, self.logfile)
 
         if arguments["--open"]:
-            try:
-                read_conf = read_file(self.config_file)
-                parsed = dict(toml_parse(read_conf))
-                editor_conf = get_key(parsed, "general", "config", "editor")
-                if editor_conf and editor_conf != {}:
-                    editor_run(editor_conf, self.config_file)
-                else:
-                    editors = ("vim", "nano", "emacs", "micro", "vi")
-                    for edt in editors:
-                        if is_tool(edt):
-                            editor_run(edt, self.config_file)
-                            break
-            except FileNotFoundError:
-                raise FileNotFoundError("File not found.")
+            open_file_with_editor(self.config_file)
         elif arguments["--view"]:
             read_config = read_file(self.config_file)
-            pydoc_pager(read_config)
+            pager(read_config)
             return True
         return False
