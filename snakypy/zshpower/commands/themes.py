9ac570bf233234d74c876a80129052100c13b15c
William C. Canin <william.costa.canin@gmail.com>
2021-11-22 03:34:15 -0300

    :hammer: Commenting code

--- snakypy/zshpower/commands/themes.py
+++ snakypy/zshpower/commands/themes.py
@@ -1,26 +1,26 @@
-from snakypy.helpers.ansi import FG
-from snakypy.helpers.console.display import printer
-
-from snakypy.zshpower.config.base import Base
-from snakypy.zshpower.utils.catch import get_zsh_theme
-from snakypy.zshpower.utils.check import checking_init
+# from snakypy.helpers.ansi import FG
+# from snakypy.helpers.console.display import printer
+#
+# from snakypy.zshpower.config.base import Base
+# from snakypy.zshpower.utils.catch import get_zsh_theme
+# from snakypy.zshpower.utils.check import checking_init
 
 
 # TODO: Version 0.12.0
-class Theme(Base):
-    def __init__(self, home):
-        Base.__init__(self, home)
-
-    def run(self, arguments) -> None:
-        checking_init(self.HOME, self.logfile)
-
-        zshrc_omz = get_zsh_theme(self.zsh_rc, self.logfile)
-
-        if not zshrc_omz:
-            return printer(
-                "Command not available. Only with Oh My ZSH installed. Aborted.",
-                foreground=FG().WARNING,
-            )
-
-        if zshrc_omz and arguments["--list"]:
-            ...
+# class Theme(Base):
+#     def __init__(self, home):
+#         Base.__init__(self, home)
+#
+#     def run(self, arguments) -> None:
+#         checking_init(self.HOME, self.logfile)
+#
+#         zshrc_omz = get_zsh_theme(self.zsh_rc, self.logfile)
+#
+#         if not zshrc_omz:
+#             return printer(
+#                 "Command not available. Only with Oh My ZSH installed. Aborted.",
+#                 foreground=FG().WARNING,
+#             )
+#
+#         if zshrc_omz and arguments["--list"]:
+#             ...
