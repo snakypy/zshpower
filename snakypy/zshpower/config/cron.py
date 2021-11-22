aae0aeddb9593a43957cc2d7efe0bcf5b543e838
William C. Canin <william.costa.canin@gmail.com>
2021-11-22 03:29:37 -0300

    :hammer: Uncommenting code to be used.

--- snakypy/zshpower/config/cron.py
+++ snakypy/zshpower/config/cron.py
@@ -1,33 +1,32 @@
-# # TODO: DEPRECATED
-# from snakypy.helpers.catches import whoami
+from snakypy.helpers.catches import whoami
 
-# from snakypy.zshpower import HOME
-# from snakypy.zshpower.config.base import Base
+from snakypy.zshpower import HOME
+from snakypy.zshpower.config.base import Base
 
-# cron_content: str = f"""
-# #!/bin/sh
+cron_content: str = f"""
+#!/bin/sh
 
-# PATH="~/.local/bin:/bin:/usr/local/bin:\\$PATH"
-# export PATH
+PATH="\\$HOME/.local/bin:/bin:/usr/local/bin:\\$PATH"
+export PATH
 
-# # Hourly in minute 0
-# # 0 * * * * {whoami()} {Base(HOME).sync_path}
+# Hourly in minute 0
+# 0 * * * * {whoami()} {Base(HOME).sync_path}
 
-# # At minute 0 past every 2nd hour
-# # 0 */2 * * * {whoami()} {Base(HOME).sync_path}
+# At minute 0 past every 2nd hour
+# 0 */2 * * * {whoami()} {Base(HOME).sync_path}
 
-# # Daily at 7:00 am
-# # 0 07 * * * {whoami()} {Base(HOME).sync_path}
+# Daily at 7:00 am
+# 0 07 * * * {whoami()} {Base(HOME).sync_path}
 
-# # You can use the site "https://crontab.guru" and some examples at "https://crontab.guru/examples.html"
-# """
+# You can use the site "https://crontab.guru" and some examples at "https://crontab.guru/examples.html"
+"""
 
-# sync_content: str = """
-# #!/bin/sh
+sync_content: str = """
+#!/bin/sh
 
-# PATH="~/.local/bin:/bin:/usr/local/bin:\\$PATH"
-# export PATH
+PATH="\\$HOME/.local/bin:/bin:/usr/local/bin:\\$PATH"
+export PATH
 
-# zshpower sync
+zshpower sync
 
-# """
+"""
