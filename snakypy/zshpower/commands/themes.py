fbf06b16daeb0fe168469cfa2faa5e1216a275ed
William C. Canin <william.costa.canin@gmail.com>
2021-11-25 01:43:33 -0300

    :hankey: Code for handling themes Oh My ZSH

--- snakypy/zshpower/commands/themes.py
+++ snakypy/zshpower/commands/themes.py
@@ -1,17 +1,18 @@
+# from subprocess import call
+#
 # from snakypy.helpers.ansi import FG
 # from snakypy.helpers.console.display import printer
 #
 # from snakypy.zshpower.config.base import Base
 # from snakypy.zshpower.utils.catch import get_zsh_theme
 # from snakypy.zshpower.utils.check import checking_init
-
-
-# TODO: Version 0.12.0
+#
+# # TODO: It will still be decided whether to implement
 # class Theme(Base):
 #     def __init__(self, home):
 #         Base.__init__(self, home)
 #
-#     def run(self, arguments) -> None:
+#     def run(self, arguments):
 #         checking_init(self.HOME, self.logfile)
 #
 #         zshrc_omz = get_zsh_theme(self.zsh_rc, self.logfile)
@@ -23,4 +24,4 @@
 #             )
 #
 #         if zshrc_omz and arguments["--list"]:
-#             ...
+#             call(["zsh", "-i", "-c", "omz theme list"])
