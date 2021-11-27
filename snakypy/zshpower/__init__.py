904d128077f05781c36a62b16fc8e2f337563028
William C. Canin <william.costa.canin@gmail.com>
2021-11-27 01:03:00 -0300

    :bookmark: Update version

--- snakypy/zshpower/__init__.py
+++ snakypy/zshpower/__init__.py
@@ -21,7 +21,7 @@ with suppress(KeyboardInterrupt):
     HOME = str(Path.home())
     __info__ = {
         "name": "ZSHPower",
-        "version": "0.11.1",
+        "version": "0.11.2",
         "description": "ZSHPower is a theme for ZSH with a manager.",
         "pkg_name": "zshpower",
         "executable": "zshpower",
