e8dd8acb74c6a05bf2c61b8437afc784524ecb56
William C. Canin <william.costa.canin@gmail.com>
2022-01-05 01:40:20 -0300

    :hammer: Update zshrc. Adding path macOS

--- snakypy/zshpower/config/zshrc.py
+++ snakypy/zshpower/config/zshrc.py
@@ -103,7 +103,7 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 #
 # PATH local user
-if [ $(uname -s) == "Darwin") ]; then
+if [[ $(uname -s) == "Darwin" ]]; then
   py_version=$(python3 -c 'import sys; sys.stdout.write(f"{{sys.version_info[0]}}.{{sys.version_info[1]}}")')
   export PATH=$PATH:$HOME/Library/Python/${{py_version}}/bin
 else
